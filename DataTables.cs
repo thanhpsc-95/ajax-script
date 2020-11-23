32684daa89c7b27298f95649742e09174973c282
thanhpsc-95 <68418110+thanhpsc-95@users.noreply.github.com>
2020-11-23 15:57:18 +0700

    Add files via upload

---
+++ DataTables.cs
@@ -0,0 +1,18 @@
+﻿using PSCPortal.DB.DTO;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace PSCPortal.Helper
+{
+    public class DataTables
+    {
+        public int draw { get; set; }
+        public int recordsTotal { get; set; }
+        public int recordsFiltered { get; set; }
+        //public List<VoteResultUser> data { get; set; }
+        public DataTable data { get; set; }
+    }
+}
